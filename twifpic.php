709a27e40b15dba627fe7dc68d257dba29772901
Justin Cano <justin.michael.cano@gmail.com>
2014-01-16 21:22:59 -0800

    friends section added

--- twifpic.php
+++ twifpic.php
@@ -1,8 +1,6 @@
 <?php
-	$twitteruser="canokiid";
 	$connection = getConnectionWithAccessToken($consumerkey, $consumersecret, $accesstoken, $accesstokensecret);
  
-
 	$user = $connection->get("https://api.twitter.com/1.1/users/show.json?screen_name=".$twitteruser); 
 
 	$pic = str_replace("_normal.jpeg",".jpeg",$user->profile_image_url);
